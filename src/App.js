df6205c6a68dd097ff7428f7baa564fb02c4de61
harigundlapally <kiranc.es.1990@gmail.com>
2021-09-27 14:24:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from '../src/components/Header';
+import Dashboard from '../src/components/Dashboard';
+import EventListing from '../src/components/EventListing';
+import Help from '../src/components/Help';
+import EventBooking from './components/EventBooking';
+import PaymentSuccess from './components/PaymentSuccess';
+import { Provider } from 'react-redux';
+import store from '../src/redux/store';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="app">
+      <Provider store={store}>
+        <BrowserRouter>
+            <Header />
+            <section className="page-wrapper">
+              <Route path='/' exact component={Dashboard} />
+              <Route path='/dashboard' component={Dashboard} />
+              <Route path='/events' component={EventListing} />
+              <Route path='/help' component={Help} />
+              <Route path='/logout' component={Dashboard} />
+              <Route path='/event-booking/:eventId' component={EventBooking} />
+              <Route path='/payment-success/:eventId' component={PaymentSuccess} />
+            </section>
+        </BrowserRouter>
+      </Provider>
+    </section>
   );
 }
 
